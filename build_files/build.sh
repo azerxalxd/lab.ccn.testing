20bb71d738c2648e1d01891b4ca414bc26e0345a
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2026-04-15 14:05:38 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,8 +4,7 @@ set -ouex pipefail
 
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 dnf5 install -y tmux 
-dnf5 install -y plasma-desktop
-dnf5 install -y sddm
+dnf5 install @kde-desktop-environment
 
 # Use a COPR Example:
 #
