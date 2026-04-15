060a8e94e12e5a48bc040fe3d9a486b0906860f7
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2026-04-15 14:35:36 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,8 +3,7 @@
 set -ouex pipefail
 
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-dnf5 install -y tmux 
-dnf5 install -y @kde-desktop-environment
+
 
 # Use a COPR Example:
 #
