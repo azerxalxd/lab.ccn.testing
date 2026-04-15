806547877462edd67b07e410ceee91a879c6476c
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2026-04-15 15:02:53 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,7 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 dnf5 -y install https://download.virtualbox.org/virtualbox/7.2.6/VirtualBox-7.2-7.2.6_172322_fedora40-1.x86_64.rpm
+dnf5 -y install libreoffice
 
 #### Example for enabling a System Unit File
 
