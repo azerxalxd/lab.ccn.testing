e8ba0356159293fa61915c6172f7473d399ea2c3
Azerxal <joshuabambridge93@gmail.com>
2026-04-16 12:18:36 +0100

    Installing dependencies for VirtualBox

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,13 @@ set -ouex pipefail
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+# Virtual Box:
+dnf5 -y install gcc
+dnf5 -y install kernel-headers
 dnf5 -y install https://download.virtualbox.org/virtualbox/7.2.6/VirtualBox-7.2-7.2.6_172322_fedora40-1.x86_64.rpm
+
+
 dnf5 -y install libreoffice
 dnf5 -y install putty
 dnf5 -y install remmina
